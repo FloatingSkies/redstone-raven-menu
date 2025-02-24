5a7e4866851ce1cf4e91420558f725b1ab3059d3
Float <111209584+FloatingSkies@users.noreply.github.com>
2025-02-24 14:25:08 -0400

    Add files via upload

--- redstone-raven-menu@floatingskies/applet.js
+++ redstone-raven-menu@floatingskies/applet.js
@@ -8,7 +8,6 @@ const Util = imports.misc.util;
 const Lang = imports.lang;
 const Clutter = imports.gi.Clutter;
 const GLib = imports.gi.GLib;
-
 const UUID = "notification-sidebar@your-name";
 
 class NotificationSidebar extends Applet.TextIconApplet {
@@ -23,9 +22,9 @@ class NotificationSidebar extends Applet.TextIconApplet {
             this.menu = new Applet.AppletPopupMenu(this, orientation);
             this.menuManager.addMenu(this.menu);
 
-            let sidebar = new St.BoxLayout({
+            this.sidebar = new St.BoxLayout({
                 vertical: true,
-                width: 380,
+                width: this.getSidebarWidth(),
                 height: global.screen_height - 50,
                 style_class: "popup-menu-content",
                 x_expand: true,
@@ -33,87 +32,112 @@ class NotificationSidebar extends Applet.TextIconApplet {
                 y_align: Clutter.ActorAlign.FILL,
             });
 
-            this.menu.box.add(sidebar);
+            this.menu.box.add(this.sidebar);
 
-
-            let title = new St.Label({
-                text: "Action Center",
-                style_class: "popup-menu-item title",
-            });
-            sidebar.add_child(title);
-
-            let notificationLabel = new St.Label({
-                text: "Latest Notification:",
-                style_class: "popup-menu-item notification-label",
-            });
-            sidebar.add_child(notificationLabel);
-
-            let notificationBox = new St.BoxLayout({
-                vertical: true,
-                style_class: "popup-menu-content notification-box",
-            });
-            let notificationText = new St.Label({
-                text: "No new notifications.",
-                style_class: "popup-menu-item notification-text",
-            });
-            notificationBox.add_child(notificationText);
-            sidebar.add_child(notificationBox);
-
-            let volumeLabel = new St.Label({
-                text: "Volume",
-                style_class: "popup-menu-item volume-label",
-            });
-            this.volumeSlider = new Slider.Slider(0.5);
-            this.volumeSlider.actor.set_style("padding: 10px; min-height: 30px;");
-            this.volumeSlider.connect("value-changed", Lang.bind(this, this.changeVolume));
-
-            sidebar.add_child(volumeLabel);
-            sidebar.add_child(this.volumeSlider.actor);
-
-            let wifiLabel = new St.Label({
-                text: "Available Wi-Fi Networks",
-                style_class: "popup-menu-item wifi-label",
-            });
-            sidebar.add_child(wifiLabel);
-
-            this.wifiBox = new St.BoxLayout({
-                vertical: true,
-                style_class: "popup-menu-content wifi-box",
-            });
-            sidebar.add_child(this.wifiBox);
-
-            this.updateWifiList();
-
-            let refreshButton = new St.Button({
-                label: "Refresh Networks",
-                style_class: "popup-menu-item refresh-button",
-            });
-            refreshButton.connect("clicked", () => {
-                this.updateWifiList();
-            });
-            sidebar.add_child(refreshButton);
-
-            let networkButton = new St.Button({
-                label: "Open Network Settings",
-                style_class: "popup-menu-item network-button",
-            });
-            networkButton.connect("clicked", () => {
-                Util.spawnCommandLine("nm-connection-editor");
-            });
-            sidebar.add_child(networkButton);
-
-            this.privacyModeButton = new St.Button({
-                label: "Privacy Mode: Disabled",
-                style_class: "popup-menu-item privacy-button",
-            });
-            this.privacyModeButton.connect("clicked", Lang.bind(this, this.togglePrivacyMode));
-            sidebar.add_child(this.privacyModeButton);
+            this.addTitle();
+            this.addNotificationSection();
+            this.addVolumeSection();
+            this.addWifiSection();
+            this.addPrivacyModeButton();
 
             this.privacyMode = false;
-
         } catch (e) {
             global.logError("Error loading the Notification Sidebar: " + e);
         }
+    }
+
+    getSidebarWidth() {
+        return global.screen_width < 1024 ? 300 : 380;
+    }
+
+    addTitle() {
+        let title = new St.Label({
+            text: "Action Center",
+            style_class: "popup-menu-item title",
+        });
+        this.sidebar.add_child(title);
+    }
+
+    addNotificationSection() {
+        let notificationLabel = new St.Label({
+            text: "Latest Notification:",
+            style_class: "popup-menu-item notification-label",
+        });
+        this.sidebar.add_child(notificationLabel);
+
+        this.notificationBox = new St.BoxLayout({
+            vertical: true,
+            style_class: "popup-menu-content notification-box",
+        });
+
+        this.notificationText = new St.Label({
+            text: "No new notifications.",
+            style_class: "popup-menu-item notification-text",
+        });
+        this.notificationBox.add_child(this.notificationText);
+        this.sidebar.add_child(this.notificationBox);
+    }
+
+    addVolumeSection() {
+        let volumeLabel = new St.Label({
+            text: "Volume",
+            style_class: "popup-menu-item volume-label",
+        });
+        this.sidebar.add_child(volumeLabel);
+
+        this.volumeSlider = new Slider.Slider(0.5);
+        this.volumeSlider.actor.set_style("padding: 10px; min-height: 30px;");
+        this.volumeSlider.connect("value-changed", Lang.bind(this, this.changeVolume));
+        this.sidebar.add_child(this.volumeSlider.actor);
+    }
+
+    addWifiSection() {
+        let wifiLabel = new St.Label({
+            text: "Available Wi-Fi Networks",
+            style_class: "popup-menu-item wifi-label",
+        });
+        this.sidebar.add_child(wifiLabel);
+
+        // Adiciona um ScrollView para a lista de redes Wi-Fi
+        this.wifiScroll = new St.ScrollView({
+            x_expand: true,
+            y_expand: true,
+            y_align: Clutter.ActorAlign.FILL,
+            style_class: "popup-menu-content wifi-scroll",
+        });
+
+        this.wifiBox = new St.BoxLayout({
+            vertical: true,
+            style_class: "popup-menu-content wifi-box",
+        });
+
+        this.wifiScroll.add_actor(this.wifiBox);
+        this.sidebar.add_child(this.wifiScroll);
+
+        this.updateWifiList();
+
+        let refreshButton = new St.Button({
+            label: "Refresh Networks",
+            style_class: "popup-menu-item refresh-button",
+        });
+        refreshButton.connect("clicked", () => this.updateWifiList());
+        this.sidebar.add_child(refreshButton);
+
+        let networkButton = new St.Button({
+            label: "Open Network Settings",
+            style_class: "popup-menu-item network-button",
+        });
+        networkButton.connect("clicked", () => Util.spawnCommandLine("nm-connection-editor"));
+        this.sidebar.add_child(networkButton);
+    }
+
+    addPrivacyModeButton() {
+        this.privacyModeButton = new St.Button({
+            label: "Privacy Mode: Disabled",
+            style_class: "popup-menu-item privacy-button",
+        });
+        this.privacyModeButton.connect("clicked", Lang.bind(this, this.togglePrivacyMode));
+        this.sidebar.add_child(this.privacyModeButton);
     }
 
     on_applet_clicked() {
@@ -164,11 +188,10 @@ class NotificationSidebar extends Applet.TextIconApplet {
     togglePrivacyMode() {
         this.privacyMode = !this.privacyMode;
         this.privacyModeButton.set_label(this.privacyMode ? "Privacy Mode: Enabled" : "Privacy Mode: Disabled");
-        this.updateWifiList(); 
+        this.updateWifiList();
     }
 }
 
 function main(metadata, orientation, panelHeight, instanceId) {
     return new NotificationSidebar(metadata, orientation, panelHeight, instanceId);
 }
-
